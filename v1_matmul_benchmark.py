89003b24672ce1747a298753fbec1dd2f926f918
Auro Tripathy <ipserv@yahoo.com>
2021-03-11 06:19:41 +0000

    working v0.0

--- v1_matmul_benchmark.py
+++ v1_matmul_benchmark.py
@@ -1,20 +1,40 @@
+# Mostly modified from https://github.com/yaroslavvb/stuff/blob/master/matmul_benchmark.py
 # On Titan X (Pascal)
-# 8192 x 8192 matmul took: 0.10 sec, 11304.59 G ops/sec
+# 16K x 16K
 # http://stackoverflow.com/questions/41804380/testing-gpu-with-tensorflow-matrix-multiplication
 
 import os
 import sys
-# import tensorflow as tf
 import time
+import argparse
 
-import tensorflow.compat.v1 as tf
+import tensorflow.compat.v1 as tf  # original code based on TF V1
 tf.disable_v2_behavior()
 
-tf.config.experimental.enable_tensor_float_32_execution(True)
+parser = argparse.ArgumentParser()
+   
+parser.add_argument('--tf32', help="tf32 execution",
+                    action='store_true')
+
+parser.add_argument('--precision', '-p', help="pick data type",
+                    type=str, required=True, choices=['fp32', 'fp16'])
+
+args = parser.parse_args()
+
+if args.tf32:
+    tf.config.experimental.enable_tensor_float_32_execution(True)
+    print('enabling tf32')
+else:
+    tf.config.experimental.enable_tensor_float_32_execution(False)
+    print('disabling tf32')
+    
 
 n = 8192 * 2
-dtype = tf.float32
-# dtype = tf.float16
+if args.precision == 'fp32':
+    dtype = tf.float32
+else:
+    dtype = tf.float16
+    
 with tf.device("/gpu:0"):
     matrix1 = tf.Variable(tf.ones((n, n), dtype=dtype))
     matrix2 = tf.Variable(tf.ones((n, n), dtype=dtype))
@@ -22,7 +42,6 @@ with tf.device("/gpu:0"):
 
 
 # avoid optimizing away redundant nodes
-# repacing with tf.compat.v1.ConfigProto
 config = tf.compat.v1.ConfigProto(graph_options=tf.compat.v1.GraphOptions(optimizer_options=tf.compat.v1.OptimizerOptions(opt_level=tf.compat.v1.OptimizerOptions.L0)))
 sess = tf.compat.v1.Session(config=config)
 
@@ -39,7 +58,10 @@ end = time.time()
 ops = n**3 + (n-1)*n**2 # n^2*(n-1) additions, n^3 multiplications
 elapsed = (end - start)
 rate = iters*ops/elapsed/10**12
-print('\n %d x %d matmul took: %.5f sec, %.2f T flops/sec' % (n, n,
+
+print('TF32:', args.tf32)
+print('Precision:', args.precision)
+print('\n %d x %d matmul took: %.5f sec, %.2f T flops' % (n, n,
                                                             elapsed/iters, 
                                                             rate,))
 
